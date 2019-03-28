7684b41787079f309a75126701d7a859a49713fb
Bodhish Thomas <bodhish@gmail.com>
2019-03-28 12:06:55 +0530

    Update .gitignore

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def facebook
-
       @user = User.from_omniauth(request.env["omniauth.auth"])
 
       if @user.persisted?
