a146109dc01f21dd4c4c8bd27b70f144c64d3652
Bodhish Thomas <bodhish@gmail.com>
2019-04-02 20:59:24 +0530

    Add basic layout for question card

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
 import Question from "./question";
+import QuestionCard from "./questionCard";
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -12,33 +13,44 @@ export default class Home extends React.Component {
       questions: props.questions
     };
   }
+
   render() {
     return (
-      <div className="container bg-black">
-        <div className="row justify-content-md-center">
-          <div className="col-12 col-md-8">
-            <div className="card bg-light mb-3">
-              <div className="card-body">
-                <div className="mr-3 rounded float-left d-block img-thumbnail">
-                  <img src={this.props.userImage} alt="photo" />
-                </div>
-                <h3>{this.props.userName}</h3>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-md-8">
-            <div className="card bg-light mb-3">
-              <div className="card-header">Questions</div>
-            </div>
-            {this.state.questions.map(question => (
-              <Question question={question.value} answers={question.answers} />
-            ))}
-          </div>
+      <div className="bg-white flex flex-col h-screen justify-center items-center text-center">
+        <div>
+          <QuestionCard />
         </div>
       </div>
     );
   }
 }
+//   render() {
+//     return (
+//       <div className="container bg-black">
+//         <div className="row justify-content-md-center">
+//           <div className="col-12 col-md-8">
+//             <div className="card bg-light mb-3">
+//               <div className="card-body">
+//                 <div className="mr-3 rounded float-left d-block img-thumbnail">
+//                   <img src={this.props.userImage} alt="photo" />
+//                 </div>
+//                 <h3>{this.props.userName}</h3>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="col-12 col-md-8">
+//             <div className="card bg-light mb-3">
+//               <div className="card-header">Questions</div>
+//             </div>
+//             {this.state.questions.map(question => (
+//               <Question question={question.value} answers={question.answers} />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 Home.propTypes = {
   userName: PropTypes.string,
---
+++ app/javascript/components/questionCard.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+export default class QuestionCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="w-full max-w-xs">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              type="text"
+              placeholder="Username"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-grey-darker text-sm font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border border-red rounded w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="******************"
+            />
+            <p className="text-red text-xs italic">Please choose a password.</p>
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Sign In
+            </button>
+            <a
+              className="inline-block align-baseline font-bold text-sm text-blue hover:text-blue-darker"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center text-grey text-xs">
+          ©2019 Acme Corp. All rights reserved.
+        </p>
+      </div>
+    );
+  }
+}
+
+{
+  /* QuestionCard.propTypes = {
+  id: PropTypes.id,
+  question: PropTypes.string,
+  answers: PropTypes.array
+}; */
+}
