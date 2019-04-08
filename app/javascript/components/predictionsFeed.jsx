70cd95408e4f9869c7ba8f68662ce8d4ee472dd3
Bodhish Thomas <bodhish@gmail.com>
2019-04-08 20:14:58 +0530

    Add animation for preditions list

--- app/javascript/components/predictionsFeed.jsx
+++ app/javascript/components/predictionsFeed.jsx
@@ -6,6 +6,7 @@ export default class PredictionsFeed extends React.Component {
     super(props);
     this.state = { latestPredictions: props.latestPredictions };
     this.fetchPredictions = this.fetchPredictions.bind(this);
+    this.cardClasses = this.cardClasses.bind(this);
   }
   componentDidMount() {
     this.timer = setInterval(() => this.fetchPredictions(), 5000);
@@ -31,15 +32,18 @@ export default class PredictionsFeed extends React.Component {
       .catch(e => console.log(e));
   };
 
+  cardClasses(bool) {
+    let classes =
+      "notification-card border border-primary-lightest my-3 flex flex-col rounded-xl shadow";
+    return bool ? classes + " notification--loaded-animate" : classes;
+  }
+
   render() {
     return (
       <div className="w-full">
-        {this.state.latestPredictions.map(prediction => {
+        {this.state.latestPredictions.map((prediction, index) => {
           return (
-            <div
-              key={prediction.id}
-              className="notification-card border border-primary-lightest my-3 flex flex-col rounded-xl shadow"
-            >
+            <div key={prediction.id} className={this.cardClasses(index == 0)}>
               <div className="flex justify-between">
                 <div className="flex w-4/5 overflow-hidden p-3">
                   <img
@@ -57,27 +61,29 @@ export default class PredictionsFeed extends React.Component {
                     )}
                     <div className="text-xs mt-2 text-left">
                       <div>
-                        <span className="mr-3">Party: {prediction.answer_1}</span>
+                        <span className="mr-3">
+                          Party: {prediction.answer_1}
+                        </span>
                         <span>P.M: {prediction.answer_2}</span>
                       </div>
-                      <div className='mt-1'>
-                        <span className="mr-3">
-                          BJP: {prediction.answer_3}</span>
+                      <div className="mt-1">
+                        <span className="mr-3">BJP: {prediction.answer_3}</span>
                         <span className="mr-3">CNG: {prediction.answer_4}</span>
-                        <span>OTH: {543 - prediction.answer_3 - prediction.answer_4}</span>
+                        <span>
+                          OTH: {543 - prediction.answer_3 - prediction.answer_4}
+                        </span>
                       </div>
                     </div>
                     <div className="text-xs text-grey mt-1">
                       {prediction.minutes_or_hours_ago}
                     </div>
                   </div>
-
                 </div>
                 <div className="flex w-1/5 flex-col text-center p-3 bg-grey-lighter justify-center items center">
                   <span>{prediction.coins_used}</span>
-                  <span className='text-xs'>coins</span></div>
+                  <span className="text-xs">coins</span>
+                </div>
               </div>
-
             </div>
           );
         })}
