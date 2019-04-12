f7bac9a93d3a1f3e56ebad843874b611a0248dbc
Bodhish Thomas <bodhish@gmail.com>
2019-04-12 20:56:53 +0530

    Fix a bug in admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,11 +5,11 @@ ActiveAdmin.register_page 'Dashboard' do
   content title: proc { I18n.t('active_admin.dashboard') } do
     div class: 'blank_slate_container', id: 'dashboard_default_message' do
       span class: 'blank_slate' do
-        h4 'Predictions Count: ' + "#{Prediction.count}"
         h4 'Total Users Count: ' + "#{User.count}"
+        h4 'Predictions Count: ' + "#{Prediction.count}"
         answer1 = Prediction.group(:answer_1).count
         answer2 = Prediction.group(:answer_2).count
-        h4 'Party: NDA:  ' + "#{answer1["NDA"] + answer1["BJP"]}" + ',  UPA:  ' + "#{answer1["UPA"] + answer1["Congress"]}"
+        h4 'Party: NDA:  ' + "#{answer1["NDA"]}" + ',  UPA:  ' + "#{answer1["UPA"]}"
         h4 'PM: Narendra Modi:  ' + "#{answer2["Narendra Modi"]}" + ',  Rahul Gandhi:  ' + "#{answer2["Rahul Gandhi"]}"
         h4 'Average Seats for BJP  :' + "#{Prediction.average(:answer_3).floor}"
         h4 'Average Seats for CONG :' + "#{Prediction.average(:answer_4).floor}"
